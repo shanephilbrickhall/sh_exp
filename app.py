2ea6e1ccc7063c381a8335e95eadc6d30cdb5101
Shane Hall <shanehall@Shanes-MBP-2.fios-router.home>
2018-09-27 13:30:01 -0400

    Sh dev commit; adding basic login route

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
 import os
 
 secret_key = os.environ.get('SECRET_KEY')
+admin_user = os.environ.get('ADMIN_USERNAME')
+admin_pass = os.environ.get('ADMIN_PASS')
 
 app = Flask(__name__)
 app.secret_key = secret_key
 
+
+@app.route('/login', methods=['POST'])
+def user_login():
+    POST_USERNAME = str(request.form['username'])
+    POST_PASSWORD = str(request.form['password'])
+    # print(POST_PASSWORD)
+
+    if POST_USERNAME == admin_user and POST_PASSWORD == admin_pass:
+        session['logged_in'] = True
+    else:
+        flash('Invalid Username or Password; you will be redirected, please revise login credentials and reattempt')
+    return hello()
 
 @app.route('/')
 def hello():
