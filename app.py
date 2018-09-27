30eedd03cbae95386362c93aa0b3cbd669be2904
Shane Hall <shanehall@Shanes-MBP-2.fios-router.home>
2018-09-27 12:52:35 -0400

    Sh dev commit; initial commit setup basic app structure

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+import os
 
+secret_key = os.environ.get('SECRET_KEY')
 
 app = Flask(__name__)
+app.secret_key = secret_key
+
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    port = int(os.environ.get('PORT', 5000))
     app.run()
