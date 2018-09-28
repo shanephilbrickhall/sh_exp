b2f935487e1a6ec8c17ca7df19f24e9539133341
Shane Hall <shanehall@Shanes-MBP-2.fios-router.home>
2018-09-27 22:43:09 -0400

    Sh dev commit; wrapping up super simple static data display

--- app.py
+++ app.py
@@ -35,11 +35,17 @@ def user_login():
 def hello():
     return "Hello World!"
 
-@app.route('/base_data_display', methods=['GET','POST'])
+@app.route('/base_data_display', methods=['GET'])
 def base_data_display():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
+
+        # These lines are simply to show that this could use reactive graphics as opposed to static files
+        # Graphics could be built using the Chart.js or Google Charts
+        # As you can see this method simply leverages the code we examined earlier which loads and grooms the data
+        # We can review the out put in the server logs for clarity
+
         base_data = pull_all_data()
 
         dates = base_data[0]
@@ -50,13 +56,57 @@ def base_data_display():
         min_tmp_data = base_data[5]
         precip_data = base_data[6]
         print(iso_real_time,iso_day_ahead,avg_tmp_data,max_tmp_data,min_tmp_data,precip_data,dates)
-        #return render_template('flat_data_chart.html', title='Base Data: ISONE RT, ISONE DA, BOST AVG TMP, '
-        #                                                'BOST HIGH TMP, BOST LOW TMP, BOST PRECIP',
-        #                       max1=170, max2=80,max3=20,labels=dates,
-        #                       values1=iso_real_time,values2=iso_day_ahead,values3=avg_tmp_data,
-        #                       values4=max_tmp_data,values5=min_tmp_data,values6=precip_data)
+        print("Below is base expirement data for display and includes the following: ISO-NE RT History, "
+              "ISO-NE Day Ahead History, Boston Average Temp History, Boston High Temp History"
+              "Boston Low Temp History, Boston Precipitation History, and the dates used for plotting them")
         return render_template('base_data_display.html')
 
+
+@app.route('/model_comparison', methods=['GET'])
+def base_data_display():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        # Again these lines are simply to show that this could use reactive graphics as opposed to static files
+        # Graphics could be built using the Chart.js or Google Charts
+        # As you can see the model is able to run on the remote server quickly and produce the expected results
+        # We can review the out put in the server logs for clarity
+
+        comp_model_data = compare_std_opt()
+
+        dates = comp_model_data[0]
+        std_model_prediction = comp_model_data[1]
+        opt_model_prediction = comp_model_data[2]
+
+        print("Below is the standard to grid search optimized comparison model run, output as follows: "
+              "Standard Model Results, Optimized Model Results, and the dates used for plotting them")
+        print(std_model_prediction,opt_model_prediction,dates)
+
+        return render_template('model_comp_display.html')
+
+
+@app.route('/scenario_comparison', methods=['GET'])
+def base_data_display():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+
+        # And again these lines are simply to show that this could use reactive graphics as opposed to static files
+        # Graphics could be built using the Chart.js or Google Charts
+        # As you can see this method also runs quickly and pulls in all the data sets and runs the optimized model
+        # again producing the expected results we can review the out put in the server logs for clarity
+        scenario_model_comp_data = scenario_build()
+
+        dates = scenario_model_comp_data[0]
+        iso_rt_hist_data = scenario_model_comp_data[1]
+        avg_scn_data = scenario_model_comp_data[2]
+        high_scn_data = scenario_model_comp_data[3]
+        low_scn_data = scenario_model_comp_data[4]
+        print("Below is the scenario comparison model run, output as follows: ISO-NE RT Historical Data, "
+              "Average Temp Scenario, High Temp Scenario, Low Temp Scenario, and the dates used for plotting them")
+        print(iso_rt_hist_data,avg_scn_data,high_scn_data,low_scn_data,dates)
+
+        return render_template('hi_low_avg_scenario.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
